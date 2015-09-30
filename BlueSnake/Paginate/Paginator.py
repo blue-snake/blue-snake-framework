1ca562770409b6ee0b13be91083b9d581a850426
David Yang <daworld.ny@gmail.com>
2015-09-30 05:10:35 +0500

    Added missing copyrights

--- BlueSnake/Paginate/Paginator.py
+++ BlueSnake/Paginate/Paginator.py
@@ -1,5 +1,12 @@
 
-# Pagination class
+ #
+ # This file is part of the BlueSnake Framework
+ # 
+ # Copyright (c) No Global State Lab
+ # 
+ # For the full copyright and license information, please view
+ # the license file that was distributed with this source code.
+ #
 
 class Paginator:
 
